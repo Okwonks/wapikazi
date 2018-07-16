a91a8070992118d103bcaaf882b740968d013ac0
Okwonks <albertoketch@gmail.com>
2018-07-16 13:44:43 +0300

    Adding import for dotenv

---
+++ config.js
@@ -0,0 +1,9 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+// Config file to store the configs needed
+module.exports = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
