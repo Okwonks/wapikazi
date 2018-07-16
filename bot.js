78dea2230a79b2b4e6a6202cfce13d6818122ac5
Okwonks <albertoketch@gmail.com>
2018-07-16 16:49:32 +0300

    completing the get function

--- bot.js
+++ bot.js
@@ -5,8 +5,27 @@ var Twit = require('twit');
 var Twitter = new Twit(config);
 
 var retweet = function () {
-    var parmams = {
+    var params = {
         q: '#wapikazi',
         result_type: 'recent',
     }
 }
+
+Twitter.get('search/tweets', params, (err, data) => {
+    if (!err) {
+        var retweet_id = data.statuses[0].id_str;
+        Twitter.post('statuses/retweet/:id', {
+            id: retweet_id
+        }, (err, res) => {
+            if (res) {
+                console.log(`retweeted ${retweet_id}`);
+            }
+            // Log whatever error could be caused.
+            if (err) {
+                console.log(`Error: ${err.message} of type ${err.name}`);
+            }
+        });
+    } else {
+        console.log(`Unable to search tweet with error: ${err.message}\n Error type ${err.name}`);
+    }
+})
