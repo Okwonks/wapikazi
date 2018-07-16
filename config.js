1d91b2b46097aa4a65b99d93f8f0dd9ab1c2a18c
Okwonks <albertoketch@gmail.com>
2018-07-16 08:52:34 +0300

    Adding configs for the bot

---
+++ config.js
@@ -0,0 +1,7 @@
+// Config file to store the configs needed
+module.exports = {
+    consumer_key: 'md3WZKscr2BqQIbPw7ke4DK44',
+    consumer_secret: '92lQ0tmrwbD6rwZZp0JTHbw4XCPLoiRi7oLne1m46GRK3ppsgQ',
+    access_token: '991969184832770048-DrAe3HKHUg1fXHAPNU1nqGUcbcQTYm1',
+    access_token_secret: 'OPkpm83eHACT9B7TsU9bWTyqoO3QYkv1kEfvEF7wx9qdV'
+}
