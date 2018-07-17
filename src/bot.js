443a88f19dd3f55ab3d9bdf889c79766f1f996a3
Okwonks <albertoketch@gmail.com>
2018-07-17 17:22:46 +0300

    commenting out posting, for now

--- src/bot.js
+++ src/bot.js
@@ -14,17 +14,17 @@ var retweet = function () {
 Twitter.get('search/tweets', {q: '#NowPlaying, #Chronology', result_type:'recent'}, (err, data) => {
     if (!err) {
         var retweet_id = data.statuses[0].id_str;
-        Twitter.post('statuses/retweet/:id', {
-            id: retweet_id
-        }, (err, res) => {
-            if (res) {
-                console.log(`retweeted ${retweet_id}`);
-            }
-            // Log whatever error could be caused.
-            if (err) {
-                console.log(`Error:[${err.name}] - ${err.message}`);
-            }
-        });
+        // Twitter.post('statuses/retweet/:id', {
+        //     id: retweet_id
+        // }, (err, res) => {
+        //     if (res) {
+        //         console.log(`retweeted ${retweet_id}`);
+        //     }
+        //     // Log whatever error could be caused.
+        //     if (err) {
+        //         console.log(`Error:[${err.name}] - ${err.message}`);
+        //     }
+        // });
     } else {
         console.log(`Unable to search tweet with error: ${err.message}\n Error type ${err.name}`);
     }
