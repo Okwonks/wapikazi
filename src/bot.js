a2be4c06348a087395d0216220318abd3204811a
Okwonks <albertoketch@gmail.com>
2018-07-23 17:27:56 +0300

    Including retweet function in the main bot

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,15 @@
 // Declaring variables to use config
 // and twit dependency from npm
 const config = require('./config');
+const qConfigs = require('./query-config'); // constants unrealted to twitter configs
 var Twit = require('twit');
 var Twitter = new Twit(config);
+var retweet = require('./tweet-utils/retweet');
+
+retweet();
+setInterval(retweet, qConfigs.rtwInterval);
+
+const stream = Twitter.stream('user');
 
 Twitter.get('search/tweets', {q: '#NowPlaying, #Chronology', result_type:'recent'}, (err, data) => {
     if (!err) {
