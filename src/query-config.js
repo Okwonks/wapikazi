582724a5c8ac70525745b2cb9a27af190fd3ba42
Okwonks <albertoketch@gmail.com>
2018-07-23 17:29:08 +0300

    including language and interval in the query-config file

--- src/query-config.js
+++ src/query-config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 module.exports = {
     queryString: process.env.QUERY_STRING,
-    resultType: process.env.RESULT_TYPE
+    resultType: process.env.RESULT_TYPE,
+    language: process.env.DEFAULT_LANG,
+    rtwInterval: process.env.RETWEET_INT
 }
