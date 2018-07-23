4d28068d6042efc36fa7e3a7111ffdb4a3875acf
Okwonks <albertoketch@gmail.com>
2018-07-23 17:27:20 +0300

    updating retweet function

--- src/tweet-utils/retweet.js
+++ src/tweet-utils/retweet.js
@@ -1,13 +1,37 @@
 const Twit = require('twit');
+const unique = require('unique-random-array');
 const config = require('../config');
 
 const param = require('../query-config');
 const bot = new Twit(config);
 
-const queryString = unique()
+const queryString = unique(param.queryString);
 
 function retweet() {
     const query = queryString();
+
+    bot.get('search/tweets', {
+        q: query,
+        result_type: param.resultType
+    }, (err, data) => {
+        if (!err) {
+            let retweet_id = data.statuses[0].id_str;
+
+            bot.post('status/retweet/:id', {
+                id: retweet_id
+            }, (err, res) => {
+                if(res) {
+                    console.log(`retweeted ${retweet_id}`);
+                }
+                if (!err) {
+                    console.log(`Retweeted: ${data.statuses[0].text}\nBy: @${data.statuses[0].user.screen_name}`);
+                }
+            });
+        } else {
+            console.log(`${err.name}: ${err.message}`);
+        }
+
+    });
 }
 
 module.exports = retweet;
