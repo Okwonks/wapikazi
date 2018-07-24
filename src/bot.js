e25f7983e007ebbe3677ad57ca124d66d3f36ceb
Okwonks <albertoketch@gmail.com>
2018-07-24 10:11:16 +0300

    Implementing retweet if only mentioned in a tweet

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,3 @@
-// Declaring variables to use config
-// and twit dependency from npm
 const config = require('./config');
 const qConfigs = require('./query-config'); // constants unrealted to twitter configs
 var Twit = require('twit');
@@ -9,30 +7,4 @@ var retweet = require('./tweet-utils/retweet');
 retweet();
 setInterval(retweet, qConfigs.rtwInterval);
 
-const stream = Twitter.stream('user');
-
-Twitter.get('search/tweets', {q: '#NowPlaying, #Chronology', result_type:'recent'}, (err, data) => {
-    if (!err) {
-        var retweet_id = data.statuses[0].id_str;
-        // Twitter.post('statuses/retweet/:id', {
-        //     id: retweet_id
-        // }, (err, res) => {
-        //     if (res) {
-        //         console.log(`retweeted ${retweet_id}`);
-        //     }
-        //     // Log whatever error could be caused.
-        //     if (err) {
-        //         console.log(`Error:[${err.name}] - ${err.message}`);
-        //     }
-        // });
-    } else {
-        console.log(`Unable to search tweet with error: ${err.message}\n Error type ${err.name}`);
-    }
-});
-// Twitter.post('statuses/update', { status: "I'm posting a tweet! @Canned_Ear" }, (err, data, response) => {
-//     console.log(data);
-    
-//     if(err) {
-//       console.log("There was a problem tweeting the message.", err);
-//     }
-//   });
+// const stream = Twitter.stream('user');
--- src/tweet-utils/mentions.js
+++ src/tweet-utils/mentions.js
@@ -4,14 +4,22 @@ const tweetEvent = (tweetObj) => {
     var json = JSON.stringify(tweetObj, null, 2);
     fs.writeFile('tweets.json', json);
 
-    var retweetTo = tweetObj.in_reply_to_screen_name;
-    var text = tweetObj.text;
-    var from = tweetObj.user.screen_name;
+    var retweetTo;
+    var text;
+    var from;
+    console.log(tweetObj.text)
+    if (tweetObj) {
+        retweetTo = tweetObj.in_reply_to_screen_name;
+        text = tweetObj.text;
+        from = tweetObj.user.screen_name;
+    }
 
     console.log(`To: ${retweetTo} from ${from}`);
     console.log(`Tweet: ${text}`);
 
-    if (retweetTo == 'WapiKazi') {
-        
+    if (retweetTo == 'wapikazi') {
+        return true;
     }
 }
+
+module.exports = tweetEvent;
