0bc5a23611f334babdc42cfb99969572879fb327
Okwonks <albertoketch@gmail.com>
2018-07-30 09:00:33 +0300

    adding event listener for the server

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,20 @@
+const { createServer } = require('http');
 const config = require('./config');
-const qConfigs = require('./query-config'); // constants unrealted to twitter configs
+const params = require('./query-config'); // constants unrealted to twitter configs
 var Twit = require('twit');
 var Twitter = new Twit(config);
 var tweet = require('./tweet-utils/tweet');
 
 tweet();
-setInterval(tweet, qConfigs.rtwInterval);
+setInterval(tweet, params.rtwInterval);
 
 // const stream = Twitter.stream('user');
+
+const server = createServer((req, res) => {
+    res.writeHead(302, {
+        Location: `https://twitter.com/${params.username}`
+    });
+    res.end()
+});
+
+server.listen(process.env.PORT || 3000);
