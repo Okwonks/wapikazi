a7a58b75c3dfd40c28fa70fc309f7601cb802063
Albert Oketch <albertoketch@gmail.com>
2018-07-31 16:27:05 +0300

    adding event listener for the server (#3)

--- src/query-config.js
+++ src/query-config.js
@@ -3,5 +3,6 @@ module.exports = {
     queryString: process.env.QUERY_STRING,
     resultType: process.env.RESULT_TYPE,
     language: process.env.DEFAULT_LANG,
-    rtwInterval: parseInt(process.env.RETWEET_INT) * 1000 * 60
+    rtwInterval: parseInt(process.env.RETWEET_INT) * 1000 * 60,
+    username: process.env.USERNAME
 }
