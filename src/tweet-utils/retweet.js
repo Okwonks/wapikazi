08f6e75c2319a315742296f91fc63a8a9d33bf79
Albert Oketch <albert@paygoenergy.co>
2022-03-21 10:52:51 +0300

    refactor: add eslint

--- src/tweet-utils/retweet.js
+++ src/tweet-utils/retweet.js
@@ -4,13 +4,13 @@ const config = require('../config');
 const bot = new Twit(config);
 
 function retweet(retweetId) {
-    bot.post('statuses/retweet/:id', {
-        id: retweetId
-    }, (err, res) => {
-        if (err) {
-            console.log(`cannot retweet ${retweetId}`);
-        }
-    });
+  bot.post('statuses/retweet/:id', {
+    id: retweetId
+  }, (err, res) => { // eslint-disable-line no-unused-vars
+    if (err) {
+      console.log(`cannot retweet ${retweetId}`);
+    }
+  });
 }
 
 module.exports = retweet;
