d86f35237397e36a9900e3b7565ca5eef01f04c4
Okwonks <albertoketch@gmail.com>
2022-09-12 23:46:54 +0300

    update comment in config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
-// Config file to store the configs needed
+// config handling access to twitter api
 module.exports = {
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
